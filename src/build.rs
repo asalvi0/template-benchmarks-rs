ba7a9c1eaa43ba1fc514edaa0edbaa6d71e4da2f
Arturo Salvio <775410+asalvi0@users.noreply.github.com>
2023-12-01 22:15:11 -0600

    updated ructe to work with latest version

--- src/build.rs
+++ src/build.rs
@@ -1,12 +1,5 @@
-use std::env;
-use std::path::PathBuf;
+use ructe::{Result, Ructe};
 
-use ructe::Ructe;
-
-fn main() {
-    let in_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("templates_ructe");
-    Ructe::from_env()
-        .unwrap()
-        .compile_templates(&in_dir)
-        .expect("compile templates");
+fn main() -> Result<()> {
+    Ructe::from_env()?.compile_templates("templates_ructe")
 }
